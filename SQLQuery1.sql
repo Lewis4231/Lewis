eacf31711f137f1b8a1815228cb6d65ff67faf72
Lewis4231 <rkddlf423123@gmail.com>
2024-07-09 21:00:06 +0900

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿create database restro
+
+create table items(
+iid int identity(1,1) primary key,
+name varchar(250) not null,
+category varchar(250) not null,
+price bigint not null
+);
+
+name,category,price
+
+select *from items
+
+truncate table items
