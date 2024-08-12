8aa672c8f33e6502cb725476c005c2a253ae3937
Lewis4231 <rkddlf423123@gmail.com>
2024-08-12 19:30:01 +0900

    Add files via upload

---
+++ CBarCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConveyorCs
+{
+    class CBarCode
+    {
+        private System.IO.Ports.SerialPort serBarCode;
+        private string barcode;
+        private bool blsSuccess;
+        public bool IsSuccess
+        {
+            get { return blsSuccess; }
+            set { blsSuccess = value; }
+        }
+        public CBarCode()
+        {
+            serBarCode = new System.IO.Ports.SerialPort();
+
+            this.serBarCode.BaudRate = 9600;
+            this.serBarCode.DataBits = 8;
+            this.serBarCode.Parity = System.IO.Ports.Parity.None;
+            this.serBarCode.PortName = "COM4";
+            this.serBarCode.StopBits = System.IO.Ports.StopBits.One;
+            this.serBarCode.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.DataReceiveCallBack);
+
+            this.serBarCode.Open();
+        }
+        private void DataReceiveCallBack(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            barcode = this.serBarCode.ReadLine();
+            blsSuccess = true;  
+        }
+        public void ReadCommand()
+        {
+            blsSuccess = false;
+            serBarCode.WriteLine("<T/>");
+        }
+        public string GetData()
+        {
+            return barcode;
+        }
+    }
+}
---
+++ CCarrier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConveyorCs
+{
+    internal class CCarrier
+    {
+        public UInt64 id;
+        public UInt64 source;
+        public UInt64 dest;
+        public USE use;
+
+        public enum USE
+        {
+            USE_NONE,
+            USE_TAKEOUT,
+            USE_STACK
+        }
+        public void Clear()
+        {
+            id = 0;
+            source = 0;
+            dest = 0;
+            use = USE.USE_NONE;
+        }
+    }
+}
