ce4e46ae70c53dd7e747f7273a18c4a0945cc0f4
Lewis4231 <rkddlf423123@gmail.com>
2024-08-30 09:27:44 +0900

    Add files via upload

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace InventoryManagement.Properties
+namespace HelloWPF.Properties
 {
 
 
